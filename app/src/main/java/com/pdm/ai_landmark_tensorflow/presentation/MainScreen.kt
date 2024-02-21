1cd9617e2c43dcc291283dae099804d8651cefb0
Pablo Molina <molinapablod@gmail.com>
2024-02-21 12:57:41 +1100

    viewModel added

--- app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/MainScreen.kt
+++ app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/MainScreen.kt
@@ -1,8 +1,86 @@
 package com.pdm.ai_landmark_tensorflow.presentation
 
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.pdm.ai_landmark_tensorflow.data.TfLiteLandmarkClassifier
+import com.pdm.ai_landmark_tensorflow.domain.Classification
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
 
+    val context = LocalContext.current
+
+    val analyzer = remember {
+        LandmarkImageAnalyzer(
+            classifier = TfLiteLandmarkClassifier(
+                context = context
+            ),
+            onResults = {
+                viewModel.updateResults(it)
+            }
+        )
+    }
+
+    val controller = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+            setImageAnalysisAnalyzer(
+                ContextCompat.getMainExecutor(context),
+                analyzer
+            )
+        }
+    }
+
+    val state by viewModel.classifications
+    MainScreenContent(state, controller)
+}
+
+@Composable
+fun MainScreenContent(
+    classifications: List<Classification>,
+    controller: LifecycleCameraController
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        CameraPreview(controller, Modifier.fillMaxSize())
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        ) {
+            classifications.forEach {
+                Text(
+                    text = it.name,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .padding(8.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/MainViewModel.kt
+++ app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/MainViewModel.kt
@@ -1,16 +1,23 @@
 package com.pdm.ai_landmark_tensorflow.presentation
 
-import androidx.camera.view.LifecycleCameraController
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
-import com.pdm.ai_landmark_tensorflow.data.TfLiteLandmarkClassifier
+import androidx.lifecycle.viewModelScope
 import com.pdm.ai_landmark_tensorflow.domain.Classification
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    val classifications: MutableState<List<Classification>> = mutableStateOf(emptyList())
+    private val _classifications: MutableState<List<Classification>> = mutableStateOf(emptyList())
+
+    val classifications: State<List<Classification>> = _classifications
+
+    fun updateResults(classification: List<Classification>) {
+        viewModelScope.launch {
+            _classifications.value = classification
+        }
+    }
 
 }
