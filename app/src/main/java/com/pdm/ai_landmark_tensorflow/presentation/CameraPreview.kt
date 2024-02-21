2fdfc201746389740dd5fd2a1fa16c6b4831d5f2
Pablo Molina <molinapablod@gmail.com>
2024-02-21 12:31:34 +1100

    ui improvements

--- app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/CameraPreview.kt
+++ app/src/main/java/com/pdm/ai_landmark_tensorflow/presentation/CameraPreview.kt
@@ -2,11 +2,24 @@ package com.pdm.ai_landmark_tensorflow.presentation
 
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.LifecycleOwner
 
 @Composable
 fun CameraPreview(
@@ -14,6 +27,7 @@ fun CameraPreview(
     modifier: Modifier
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
+    val primaryColor = MaterialTheme.colorScheme.primary
 
     AndroidView(
         factory = {
@@ -23,4 +37,22 @@ fun CameraPreview(
             }
         }, modifier = modifier
     )
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        val blurRadius = 25f
+        val squareSize = size.minDimension * 0.8f
+        val squareTopLeft = Offset((size.width - squareSize) / 2, (size.height - squareSize) / 2)
+
+        drawRoundRect(
+            color = primaryColor,
+            topLeft = Offset(squareTopLeft.x - blurRadius, squareTopLeft.y - blurRadius),
+            size = Size(squareSize + blurRadius * 2, squareSize + blurRadius * 2),
+            style = Stroke(width = 6.dp.toPx()),
+            cornerRadius = CornerRadius(50f, 50f)
+        )
+    }
 }
